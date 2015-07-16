eca8d27b91b32e8bc4dedb7f0775775643422a1f
travish <dev@traviswhill.com>
2015-07-16 08:43:26 -0500

    Fix install.php for development and production environments.

--- public_html/lib/install.php
+++ public_html/lib/install.php
@@ -31,7 +31,12 @@ function wp_install_defaults( $user_id )
     global $wpdb, $wp_rewrite, $current_site, $table_prefix;
 
     // Fix upload directory location.
-    update_option( 'upload_path', untrailingslashit( str_replace( 'core', 'lib/uploads', ABSPATH ) ) );
+    if (WP_ENV == 'dev'){
+        update_option( 'upload_path', untrailingslashit( str_replace( 'core', 'lib\uploads', ABSPATH ) ) );
+    } else {
+        update_option( 'upload_path', untrailingslashit( str_replace( 'core', 'lib/uploads', ABSPATH ) ) );
+    }
+    
     update_option( 'upload_url_path', home_url( '/lib/uploads' ) );
     
 }
